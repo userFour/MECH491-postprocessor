38aaa2a43101d076c933968b1b5824a64ccfdf36
pkosy <94577165+pkosy@users.noreply.github.com>
2021-11-21 14:21:16 -0800

    Update post.py
    Now it works with A and C axis output!

--- post.py
+++ post.py
@@ -9,7 +9,7 @@ Created on Fri Oct 15 10:33:10 2021
 import numpy as np 
 import matplotlib.pyplot as plt
 from matplotlib import cm
-import math as math
+from math import *
 import time as time
 import sys as sys
 import datetime
@@ -36,40 +36,25 @@ for i in range(0,len(lines)):
     
     #GOTO/ x, y [,z [,i ,j ,k] [,feed]]
     if(currentCommand[0:4] == "GOTO"):
-        currentContent[0] = currentCommand[5:len(currentCommand)]
-        gotoPrefixes = ['X', 'Y', 'Z', 'I', 'J', 'K', 'F']
+        currentContent[0] = currentCommand[5:len(currentCommand)] # Removes "GOTO/" prefix
+        gotoPrefixes = ['X', 'Y', 'Z', 'A', 'C', '', 'F']
+        
+        if (len(currentContent[:]) >= 4): # this means that we have i j k values that need processing
+            v = np.array([currentContent[3], currentContent[4], currentContent[5]], dtype=float)
+            theta_A = acos(v[2])
+            theta_C = atan2(v[0], v[1])
+            currentContent[3] = str(round(degrees(theta_A),4))
+            currentContent[4] = str(round(degrees(theta_C),4))
+            currentContent[5] = "";
+        
+        
         
         f2.write("N"+str((i+1)*5))
         for i in range(0,len(currentContent[:])):
-            f2.write(str(" " + gotoPrefixes[i])+currentContent[i])
+            if(currentContent[i]!= ""):
+                f2.write(str(" "))
+            f2.write(str(gotoPrefixes[i])+currentContent[i])
         f2.write("\n")
 
-
-
-V = [0, 0, 1]
-
-
-A = math.pi/180 * -27.54332457
-B = math.pi/180 * -25.40287102
-C = 0
-
-
-Rx =[[1, 0, 0],
-     [0, math.cos(A), -math.sin(A)],
-     [0, math.sin(A),  math.cos(A)]]
-
-Ry =[[math.cos(B), 0, math.sin(B)],
-     [0, 1, 0],
-     [-math.sin(B), 0, math.cos(B)]]
-
-Rz =[[math.cos(C), -math.sin(C), 0],
-     [math.sin(C),  math.cos(C), 0],
-     [0, 0, 1]]
-
-
-V1 = np.matmul(Rx, V)
-V2 = np.matmul(Ry, V1)
-V3 = np.matmul(Rz, V2)
-print(V3)
-
+        
 f2.close()
