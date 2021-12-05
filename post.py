a191117e393a4ec9402a6b723ff8c7e2ac00d0c2
userFour <userthirtyone@gmail.com>
2021-12-04 21:27:15 -0800

    implemented line numbers, circles still broken

--- post.py
+++ post.py
@@ -22,6 +22,8 @@ import datetime
 
 # Define some variables
 lines = []
+conversionFactor = 25.4
+printFlag = False
 lineNumber = 0 # Make sure first line number is 5
 now = datetime.datetime.now()
 outFile = str(now.strftime("%Y.%m.%d.%H.%M.%S")) + " Output G code.txt"
@@ -29,43 +31,73 @@ outLine = ""
 rapidNextLine = 0
 currentToolPosition = ['', '', '', '', ''] # X, Y, Z, B, C
 
-# Start the program
+# Functions
+# -----------------------------------------------------------------------------
+# Remove redundant positions - TODO
+def removeRedundantPosiotionValues(data, currentToolPosition):
+    #TODO
+    
+    return (data)
+
+# -----------------------------------------------------------------------------
+# Increment line numbers and output text to file
+def writeToFile(writeData, num):
+    
+    printString = "N" + str(num)
+    for i in range(len(writeData)):
+        if(writeData[i] != None):
+            printString += " " + str(writeData[i])
+    
+    f2.write("\n" + printString)
+
+    return ()
+
+# --------------------- START PROGRAM ---------------------
 os.system('cls' if os.name == 'nt' else 'clear')
 print("Start program!")
 print("Directory: ", end="")
 print(os.getcwd())
 
-# Gcode header block - TODO
-#
-#
-
 # Read input data from CLDATA file
-with open('APT_SimpleMillPlane.txt') as f:
+with open('APT_Zig_Zag_Smoothed.txt') as f:
     lines = f.readlines()
     f.close()
 
 # Create the output file and open it in append mode
 f2 = open(outFile, 'a')
+print("Filename: " + outFile)
+
+# Gcode header block <-- TODO: Get startup parameters from input CLDATA
+f2.write(";START PROGRAM")
+f2.write("\nG20 G90 ;Programming in inches, absolute coordinates")
+f2.write("\nG28 X Y Z")
+f2.write("\nM03 S2037 ;Spindle on (clockwise), 2037 rpm")
+f2.write("\nM08 ;Coolant on (flood)")
+f2.write("\n;Begin cutting operations")
+#
 
 # Start parsing the CLDATA and output Gcode
-for i in range(0,len(lines)):
+for i in range(0, len(lines)):
     
     currentContent = lines[i].split(",")
     currentCommand = currentContent[0]
+
+    # Clear the writeData list
+    writeData = [None] * 12 # (11 is max number of parmeters (circle cmd))
     
     # GOTO/ x, y [,z [,i ,j ,k] [,feed]]
     if(currentCommand[0:4] == "GOTO"):
-        currentContent[0] = currentCommand[5:len(currentCommand)] # Removes "GOTO/" prefix
-        gotoPrefixes = ['X', 'Y', 'Z', 'B', 'C', '', 'F']
 
-        # Increment line number, build the string
-        lineNumber += 5
-        outLine = "N" + str(lineNumber)
+        printFlag = True
+
+        currentContent[0] = currentCommand[5:len(currentCommand)] # Removes "GOTO/" prefix
+        gotoPrefixes = ['X', 'Y', 'Z', 'B', 'C', ' ', 'F']
+
         if (rapidNextLine == 1):
-            outLine += " G00"
+            writeData[0] = "G00"
             rapidNextLine = 0
         else:
-            outLine += " G01"
+            writeData[0] = "G01"
         
         # Calculate B and C axis position
         if (len(currentContent[:]) >= 4): # If we have i j k values that need processing
@@ -78,35 +110,27 @@ for i in range(0,len(lines)):
   
         # Loop over each element
         for i in range(0, len(currentContent[:])):
-            if(currentContent[i]!= ""): # If there is data in the cell
-                outLine += " "
-                outLine += str(gotoPrefixes[i]) + str(round(float(currentContent[i])/25.4, 4))
-        
-        # Print the output Gcode line
-        outLine += "\n"
-        # print(outLine, end="")
-        f2.write(outLine)
-        
-
-        
+            if(currentContent[i] != ""): # If there is data in the cell
+                # Needs to be (i+1) cause index 0 is for G-command
+                writeData[i+1] = str(gotoPrefixes[i]) + str(round(float(currentContent[i])/conversionFactor, 4))
 
     # CIRCLE/
     if(currentCommand[0:6] == "CIRCLE"):
+
+        printFlag = True
+
         currentContent[0] = currentCommand[7:len(currentCommand)] # Removes "CIRCLE/" prefix
         circlePrefixes = ['X', 'Y', 'Z', ' ', ' ', ' ', 'I', 'J', 'K', ' ', ' ']
         
-        # Increment line number, build the string
-        lineNumber += 5
-        outLine = "N" + str(lineNumber) + " G03 "
+        # Logic to assign G02/G02 - Needs generalization to function in XZ, YZ planes
+        if(float(currentContent[5]) > 0):
+            writeData[0] = "G02"
+        else:
+            writeData[0] = "G03"
 
         for i in range(len(circlePrefixes)):
             if(circlePrefixes[i] != ' '):
-                outLine += str(circlePrefixes[i]) + str(round(float(currentContent[i])/25.4, 4)) + ' '    
-        
-        # Print the output Gcode line
-        outLine += "\n"
-        # print(outLine, end="")
-        f2.write(outLine)
+                writeData[i+1] = str(circlePrefixes[i]) + str(round(float(currentContent[i])/conversionFactor, 4)) + ' '   
 
 
     # FEDRAT/MMPM, f
@@ -117,33 +141,27 @@ for i in range(0,len(lines)):
         outLine += str(round(float(Feed)/25.4, 1))
 
         # Increment line number, build the string
-        lineNumber += 5
-        outLine = "N" + str(lineNumber) + " F " + str(Feed)
+        # lineNumber += 5
+        # outLine = "N" + str(lineNumber) + " F " + str(Feed)
             
         # Print the output Gcode line
         # print(outLine, end="")
-        f2.write(outLine)
+        # f2.write(outLine)
 
 
     # RAPID
     if(currentCommand[0:5] == "RAPID"): # the next line will have a GOTO command that should be G00
         rapidNextLine = 1
 
+    # Send the data to the file write function
+    if(printFlag):
+        lineNumber += 5
+        writeToFile(writeData, lineNumber)
+        printFlag = False
+
+# End main loop
+
 f2.close()
-##os.remove(outFile) # Uncomment this line to supress output file
+#os.remove(outFile) # Uncomment this line to supress output file
 
 print("End program!\n")
-
-# -----------------------------------------------------------------------------
-# Remove redundant positions - TODO
-def removeRedundantPosiotionValues(data, currentToolPosition):
-    #TODO
-    
-    return (data)
-
-# -----------------------------------------------------------------------------
-# Increment line numbers and output text to file
-def writeToFile(inString):
-    #TODO
-    
-    return ()
